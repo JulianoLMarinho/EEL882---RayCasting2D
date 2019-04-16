8b720b5e914b6a78a2bd3a38dcc8a07f4a74cf40
Juliano Marinho <j.delimamarinho@gmail.com>
2019-04-15 23:37:39 -0300

    testando os raios

--- sketch.js
+++ sketch.js
@@ -30,17 +30,6 @@ class Shapes{
                 inter++;
             }
         }
-        let ab1 = this.ab(this.points[0][0], this.points[0][1], this.points[this.points.length-1][0], this.points[this.points.length-1][1]);
-        let ab2 = this.ab(this.lines[0], this.lines[1], mouseX, mouseY);
-        let x = (ab2[1]-ab1[1])/(ab1[0]-ab2[0]);
-        let y = (ab1[0]*x+ab1[1]);
-        if(x>Math.min(this.points[0][0],this.points[this.points.length-1][0]) &&
-            x<Math.max(this.points[0][0],this.points[this.points.length-1][0]) &&
-            y>Math.min(this.points[0][1],this.points[this.points.length-1][1]) &&
-            y<Math.max(this.points[0][1],this.points[this.points.length-1][1]) && x <= mouseX && y <= mouseY
-        ) {
-            inter++;
-        }
         if (inter%2==0){
             return false;
         } else {
@@ -50,19 +39,26 @@ class Shapes{
 
     }
 
-    addPoint(){
+    addPoint(lastPoint = false){
         if(this.stillDraw) this.points.push([mouseX, mouseY]);
+        if(lastPoint) this.points.push([this.points[0][0],this.points[0][1]]);
     }
 
     ab(x1,y1,x2,y2){
         let a = ((y1-y2)/(x1-x2));
-        let b = y2-(x2*a);
+        let b = y2+(x2*a);
         return [a,b];
     }
 }
 
+function ab(x1,y1,x2,y2){
+    let a = ((y2-y1)/(x2-x1));
+    let b = y2-(x2*a);
+    return [a,b];
+}
 
 let s = [];
+let rays = [];
 
 function setup() { 
     createCanvas(800, 800);
@@ -72,23 +68,26 @@ function setup() {
 function draw() { 
     background(220);
 
-    strokeWeight(1);
-    for(let sh of s){
-        if(sh.points.length>1){
-            if(sh.mouseDentro() && !sh.stillDraw){
-                strokeWeight(3);
-            } else {
-                strokeWeight(1);
-            }
-        }
-        sh.desenhaShape();
-    }
+    strokeWeight(10);
+    // for(let sh of s){
+    //     if(sh.points.length>1){
+    //         if(sh.mouseDentro() && !sh.stillDraw){
+    //             strokeWeight(3);
+    //         } else {
+    //             strokeWeight(1);
+    //         }
+    //     }
+    //     sh.desenhaShape();
+    // }
 
+    point(400,400);
+    strokeWeight(2);
+    let j = ab(400, 400, mouseX, mouseY);
+    line(400,400, 810, 810*j[0]+j[1]);
     
 }
 
 function mousePressed() {
-    console.log(mouseX, mouseY);
     if(s.length>0 && s[s.length-1].stillDraw){
         s[s.length-1].addPoint();
     } else {
@@ -99,6 +98,7 @@ function mousePressed() {
 
 function doubleClicked() {
     if(s.length>0){
+        s[s.length-1].addPoint(true);
         s[s.length-1].stillDraw = false;
     }
     
